913586e32630eea204bf8f61ab0c07219de94d2c
Richard Janssen <richard.janssen.93@gmail.com>
2022-04-23 11:54:26 +0200

    Update controller route

--- PhotographyApi/Controllers/WeatherForecastController.cs
+++ PhotographyApi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace PhotographyApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("PhotographyApi/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
