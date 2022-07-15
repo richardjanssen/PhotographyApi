9b10dc3621dc1d1d9d0f01893786bc5a02e973f3
Richard Janssen <richard.janssen.93@gmail.com>
2022-07-15 07:55:32 +0200

    Resolve some formatting improvements

--- Test.Helpers/TestConstants.cs
+++ Test.Helpers/TestConstants.cs
@@ -2,5 +2,5 @@
 
 public static class TestConstants
 {
-    public static DateTime SomeDateTime = new DateTime(2022, 07, 05, 20, 12, 45);
+    public static readonly DateTime SomeDateTime = new(2022, 07, 05, 20, 12, 45);
 }
