d5b419603abf42bb8d53368d570d63f0ac757ee7
Richard Janssen <richard.janssen.93@gmail.com>
2022-09-15 19:43:48 +0200

    Add HealthController

---
+++ PhotographyApi/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhotographyApi.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]/[action]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public string Check() => "Ok";
+}
