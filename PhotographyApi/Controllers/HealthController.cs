a99d3efc2300e520b3dc55cb59c8c101af1b06a5
Richard Janssen <richard.janssen.93@gmail.com>
2022-09-18 14:15:33 +0200

    Add file logging

--- PhotographyApi/Controllers/HealthController.cs
+++ PhotographyApi/Controllers/HealthController.cs
@@ -6,6 +6,17 @@ namespace PhotographyApi.Controllers;
 [Route("api/v1/[controller]/[action]")]
 public class HealthController : ControllerBase
 {
+    private readonly ILogger<HealthController> _logger;
+
+    public HealthController(ILogger<HealthController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet]
-    public string Check() => "Ok!";
+    public string Check()
+    {
+        _logger.LogInformation("Call to HealthController - Check");
+        return "Ok!";
+    }
 }
