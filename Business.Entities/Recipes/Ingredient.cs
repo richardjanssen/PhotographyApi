95c1b9140c4fd5a03ba2acbcf56c7912f2744d2d
Richard Janssen <github@rjanssen.net>
2026-08-01 21:27:40 +0200

    Setters nodig voor EF

--- Business.Entities/Recipes/Ingredient.cs
+++ Business.Entities/Recipes/Ingredient.cs
@@ -3,10 +3,10 @@
 namespace Business.Entities.Recipes;
 public class Ingredient(string name, string quantity, string? unit, string? subgroup) : EntityBase
 {
-    public string Name { get; } = name;
-    public string Quantity { get; } = quantity;
-    public string? Unit { get; } = unit;
-    public string? Subgroup { get; } = subgroup;
+    public string Name { get; private set; } = name;
+    public string Quantity { get; private set; } = quantity;
+    public string? Unit { get; private set; } = unit;
+    public string? Subgroup { get; private set; } = subgroup;
 
     // Parameterless constructor for EF Core
     public Ingredient() : this(string.Empty, string.Empty, null, null) { }
